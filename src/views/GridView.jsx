bf501b7decb3cfb712e9665d28ca0d02db24204f
unknown <enzoperaltalio@gmail.com>
2025-03-21 22:10:39 -0300

    commit despues de agregar grid principal y grilla

--- src/views/GridView.jsx
+++ src/views/GridView.jsx
@@ -13,6 +13,11 @@ import react from "/icons/react.svg";
 import mongodb from "/icons/mongodb.svg";
 import node from "/icons/nodejs.svg";
 import docker from "/icons/docker.svg";
+import tailwind from "/icons/tailwind.svg";
+import python from "/icons/python.svg";
+import express from "/icons/express.svg";
+import bootstrap from "/icons/bootstrap.svg";
+import git from "/icons/git.svg";
 
 const GridView = () => {
   const explicitTheme = {
@@ -25,20 +30,19 @@ const GridView = () => {
     react,
     mongodb,
     node,
+    git,
     docker,
-    ts,
-    js,
-    react,
-    ts,
-    js,
-    react,
+    tailwind,
+    python,
+    express,
+    bootstrap,
   ];
 
   return (
-    <section className="max-h-max max-w-screen sm:max-w-[60%] sm:m-auto relative p-[4%] z-10">
-      <section className=" items-center justify-center">
-        <article className="h-[420px] rounded-xl border border-slate-700"></article>
-        <ul className="h-40 flex justify-between items-center">
+    <section>
+      <section className="max-w-[90%] sm:max-w-[65%] sm:h-[800px] sm:grid items-center justify-center gap-4 sm:grid-cols-4 sm:grid-rows-3 m-auto">
+        <article className="h-[420px] sm:relative sm:top-[70px] sm:h-[300px] sm:col-span-2 rounded-xl border border-slate-700"></article>
+        <ul className="h-40 sm:w-[362px] sm:top-[70px] flex sm:relative justify-between items-center">
           <li className="h-28 w-28 border p-10 border-slate-700 rounded-xl flex items-center justify-center">
             <img src={x} alt="" />
           </li>
@@ -49,7 +53,7 @@ const GridView = () => {
             <img src={instagram} alt="" />
           </li>
         </ul>
-        <article className="h-72 max-w-screen border p-5 border-slate-700 rounded-xl flex flex-col justify-center">
+        <article className="h-72 sm:h-[430px] sm:relative sm:top-[135px] sm:col-start-3 sm:row-start-1 sm:col-span-2 max-w-screen border p-5 border-slate-700 rounded-xl flex flex-col justify-center">
           <article className="relative bottom-10  w-max px-4 py-2 flex items-center gap-2 border border-slate-600 rounded-3xl">
             <img src={job} alt="" />
             <h2 className="text-slate-200 text-xs font-semibold ">
@@ -62,7 +66,7 @@ const GridView = () => {
             debitis ipsam. Expedita sequi praesentium
           </h2>
         </article>
-        <button className="h-20 border border-slate-700 mt-5 rounded-xl flex justify-between w-[100%] items-center px-5 text-xl font-semibold text-[#ccc]">
+        <button className="h-20 sm:h-24 border sm:relative  border-slate-700 mt-5 rounded-xl sm:col-start-1 col-span-2  relative flex justify-between w-[100%] items-center px-5 text-xl font-semibold text-[#ccc]">
           Discover more projects
           <img
             className="hover:rotate-90 hover:scale-125 transition-all"
@@ -70,7 +74,7 @@ const GridView = () => {
             alt=""
           />
         </button>
-        <article className="border border-slate-700 rounded-xl p-4 mt-5">
+        <article className="border sm:relative sm:top-3 border-slate-700 sm:h-52 rounded-xl p-4 mt-5 sm:col-span-2 sm:row-start-3">
           <GitHubCalendar
             username="enzoolionel"
             theme={explicitTheme}
@@ -78,7 +82,7 @@ const GridView = () => {
             year={2025}
           />
         </article>
-        <article className="h-80 mt-5 max-w-screen border p-3 border-slate-700 rounded-xl flex flex-col text-[#ccc] justify-between">
+        <article className="h-80 sm:relative sm:top-20 mt-5 sm:mt-0 max-w-screen border p-3 border-slate-700 rounded-xl sm:col-start-3 sm:col-span-2 sm:row-start-3 flex flex-col text-[#ccc] justify-between">
           <div className="relative w-max px-4 py-2 flex gap-2 items-center border border-slate-600 rounded-3xl">
             <img src={stack} alt="" />
             <h2 className="text-xs font-semibold ">Tech Stack</h2>
@@ -90,11 +94,11 @@ const GridView = () => {
               animate={{ x: ["0%", "-50%"] }} // Mueve los elementos solo a la mitad de su tamaño
               transition={{
                 repeat: Infinity,
-                duration: 13,
+                duration: 15,
                 ease: "linear",
               }}
             >
-              {[...iconos, ...iconos].map((icon, index) => (
+              {[...iconos, ...iconos, ...iconos].map((icon, index) => (
                 <img className="h-[70px]" src={icon} key={index} alt="" />
               ))}
             </motion.div>
