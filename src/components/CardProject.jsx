f4e13ae9255a09c6c2bf1b33ba9697803628b481
unknown <enzoperaltalio@gmail.com>
2025-03-26 10:53:49 -0300

    commit despues de agregar estilos a la cardProject

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,17 +1,17 @@
 import { motion } from "framer-motion";
 import razor from "/images/razor.png";
 import tailwind from "/icons/tailwind.svg";
-import python from "/icons/python.svg";
+import react from "/icons/react.svg";
 import express from "/icons/express.svg";
-import bootstrap from "/icons/bootstrap.svg";
-import git from "/icons/git.svg";
+import node from "/icons/nodejs.svg";
+import mongo from "/icons/mongodb.svg";
 
 const CardProject = () => {
-  const tools = [tailwind, python, express, bootstrap, git];
+  const tools = [react, express, node, mongo, tailwind];
 
   return (
     <motion.article
-      className="z-10 flex flex-col gap-3 mt-7"
+      className="z-10 flex flex-col gap-3 mt-7 p-4 rounded-2xl shadow"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
@@ -19,11 +19,11 @@ const CardProject = () => {
       <motion.img
         src={razor}
         alt="imagen"
-        className="hover:scale-105 transition-all overflow-hidden h-[230px] w-full object-cover rounded-2xl"
+        className="overflow-hidden h-[230px] w-full rounded-2xl"
         whileHover={{ scale: 1.05 }}
         initial={{ scale: 1 }}
         animate={{ scale: 1 }}
-        transition={{ duration: 0.3 }}
+        transition={{ duration: 0.05 }}
       />
       <div className="flex justify-between items-center w-full">
         <h2 className="font-semibold text-white">2024</h2>
@@ -35,34 +35,36 @@ const CardProject = () => {
               alt="icono"
               className="w-7 h-7 object-cover"
               whileHover={{ scale: 1.2 }}
-              transition={{ duration: 0.2 }}
+              transition={{ duration: 0.05 }}
             />
           ))}
         </ul>
       </div>
       <motion.h1
-        className="text-2xl font-semibold text-white"
+        className="text-3xl font-semibold text-white"
         initial={{ y: 20, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
-        Título del Proyecto
+        Barberia Razor
       </motion.h1>
       <motion.h2
-        className="text-slate-500"
+        className="text-slate-500 max-w-[300px]"
         initial={{ x: -20, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
-        Datos del proyecto, detalles y más información...
+        Lanzado en marzo de 2025, este proyecto utiliza React, Express, Node.js,
+        MongoDB y Tailwind CSS .
       </motion.h2>
-      <motion.button
-        className="w-max text-green-500 rounded-lg mt-4 font-semibold"
-        whileHover={{ scale: 1.1 }}
-        transition={{ duration: 0.2 }}
+      <motion.a
+        href="https://barbersystem.onrender.com/"
+        className="w-max text-white rounded-lg mt-4 font-semibold px-4 py-2 hover:cursor-pointer bg-green-800 border-green-500"
+        whileHover={{ scale: 1.05 }}
+        transition={{ duration: 0.05 }}
       >
         Ver más!
-      </motion.button>
+      </motion.a>
     </motion.article>
   );
 };
--- src/layout/ProjectsLayout.jsx
+++ src/layout/ProjectsLayout.jsx
@@ -4,21 +4,27 @@ import job from "/icons/job.svg";
 const ProjectsLayout = () => {
   return (
     <section className="min-w-screen min-h-screen ">
-      <section className="w-screen sm:w-70% sm:max-w-[70%] lg:max-w-[80%] xl:max-w-[50%] m-auto  flex flex-col gap-5 p-5 mt-60 mb-32">
+      <section className="w-screen sm:w-70% sm:max-w-[70%] lg:max-w-[85%] xl:max-w-[50%] m-auto  flex flex-col gap-5 p-5 mt-60 mb-32">
         <article className="w-max px-4 py-2 flex items-center opacity-80 gap-2 border bg-green-900 border-green-500 rounded-2xl">
           <img src={job} alt="" />
           <h2 className="text-slate-200 text-xs font-semibold ">Proyectos</h2>
         </article>
         <article className="py-10 flex gap-4 justify-between">
-          <div className="flex flex-col gap-3">
+          <div className="max-w-[550px] flex flex-col gap-3">
             <h1 className="text-white text-4xl font-semibold">
-              Titulo del proyecto
+              Algunos de mis proyectos
             </h1>
             <h2 className="text-slate-500">
-              Este es un proyecto X tiene un servidor con diferentes tecnologias
+              Desarrollo de soluciones modernas con{" "}
+              <span className="text-green-400 font-semibold">
+                React, Node.js y MongoDB
+              </span>
+              . Cada proyecto es una oportunidad para aprender, mejorar y
+              construir algo útil. Explora algunos de los proyectos en los que
+              he trabajado recientemente.
             </h2>
           </div>
-          <article className="h-13 px-5 sm:flex items-center gap-2 border border-slate-200 rounded-4xl hidden">
+          <article className="h-13 px-5 sm:flex items-center gap-2 border border-slate-200 rounded-4xl hidden hover:cursor-pointer hover:scale-105 transition-all">
             <img src={job} alt="" />
             <h2 className="text-slate-200 text-xs font-semibold ">
               Proyectos Destacados
